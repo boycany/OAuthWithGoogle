95612f237083f3425e3488cca8f58e0e53b2c4b4
111084 <111084@w.tmu.edu.tw>
2022-06-27 09:59:27 +0800

    ch31-11 conditional nav done

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 //創建一個叫 authCheck 的 Middleware
 const authCheck = (req, res, next) => {
-  console.log(req.user);
+  console.log("req.user in authCheck Middleware: >>", req.user);
   if (!req.isAuthenticated()) {
     res.redirect("/auth/login");
   } else {
